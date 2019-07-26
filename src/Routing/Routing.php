285e6cc2793830f7c42b5abb2e3cb01b45d168c7
mmfjunior1 <mmfjunior1@gmail.com>
2019-07-26 13:13:29 -0300

    Update Routing.php

--- src/Routing/Routing.php
+++ src/Routing/Routing.php
@@ -177,7 +177,7 @@ class Routing
         try {
             return self::_submit();
         } catch (\Exception $e) {
-            return \App\Utils\View\View::json(array("msg"=>'Exception error: '.  $e->getMessage(),"status" => false));
+            return \FastApi\View\ViewView::json(array("msg"=>'Exception error: '.  $e->getMessage(),"status" => false));
         }
     }
 }
